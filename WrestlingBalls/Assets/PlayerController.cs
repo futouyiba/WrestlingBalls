b04267a40c4c1c1f1667a1132e53716a2f4dd806
futouyiba <futouyiba@gmail.com>
2016-09-20 04:03:59 +0800

    Init

---
+++ WrestlingBalls/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+	public float speed=20;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		if (isLocalPlayer)
+			gameObject.GetComponent<MeshRenderer> ().material.color = Color.red;
+	}
+
+	void FixedUpdate ()
+	{
+		if (!isLocalPlayer)
+			return;
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+}
